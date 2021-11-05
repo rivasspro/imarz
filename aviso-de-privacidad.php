18c652537d2b9a6973e58a40b5e46c5a650ed49f
rivasspro <david22rivas@gmail.com>
2021-11-05 16:39:49 -0600

    add aviso de privacidad

---
+++ aviso-de-privacidad.php
@@ -0,0 +1,55 @@
+<?php
+$m_productos = true;
+$m_actual = 'home';
+include('encabezados.php');
+?>
+
+<section id="aviso-de-privacidad" class="bn_container py-nav min-500 bg-light">
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h4>Aviso de privacidad</h4>
+                <h4>MIGUEL ANGEL RIVERA ZAVALA, en lo subsecuente iMARZ, con domicilio en Chilpancingo No. 59 Mezzanine 2, Col. Hipódromo, Alcaldía Cuahutémoc, Cidudad de México, C.P. 06100 , es responsable del tratamiento de sus datos personales.</h4>
+
+                <h5>
+                    Sus datos personales serán utilizados para las siguientes finalidades:
+                </h5>
+
+                <p> Proveer los servicios y productos requeridos por usted, Elaborar y enviar sus facturas electrónicas, Informar sobre cambios o nuevos productos o servicios que estén relacionados con el contratado adquirido por el cliente, Dar cumplimiento a obligaciones contraídas con nuestros clientes, Evaluar la calidad del servicio, Realizar estudios internos sobre hábitos de consumo.
+                </p>
+                <h5>Para las finalidades señaladas en el presente aviso de privacidad, podemos recabar sus datos personales de distintas formas:</h5>
+
+                <p>Cuando usted nos los proporciona directamente, Cuando visita nuestro sitio de internet o utiliza nuestros servicios en línea, Cuando obtenemos información a través de otras fuentes que están permitidas por la ley.</p>
+
+                <h5>Datos personales que recabamos de forma directa:</h5>
+
+                <p> Razón social ó Nombre para personas físicas, Nombre completo del contacto, RFC, Dirección fiscal completa, Correo electrónico de la empresa y/o del contacto, Número telefónico</p>
+
+                <p>Datos personales que recabamos cuando visita nuestro sitio de internet o utiliza nuestros servicios en línea.</p>
+
+                <p>Razón social ó Nombre para personas físicas, Nombre completo del contacto, Correo electrónico de la empresa y/o del contacto, Número telefónico</p>
+
+                <h5>Datos personales que recabamos a través de otras fuentes, podemos obtener información de usted de otras fuentes permitidas por la ley, tales como los directorios telefónicos, empresariales o laborales. Los datos que obtenemos por estos medios pueden ser:</h5>
+
+                <p>Correo electrónico, Razón social ó Nombre para personas físicas, Nombre completo del contacto, Número telefónico.
+                </p>
+                <p> Le informamos que ninguna de esta información es considerada como Datos Sensibles en los términos señalados en la Fracción VI del Artículo 3ro. De la Ley Federal de Protección de Datos Personales en Posesión de los Particulares.</p>
+
+                <p>Usted puede dejar de recibir mensajes informativos o promocionales vía correo electrónico, al final de cada correo electrónico que usted recibe existen los mecanismos necesarios para la cancelación de estos envíos.
+                </p>
+                <p>Usted tiene derecho de acceder, rectificar y cancelar sus datos personales, así como de oponerse al tratamiento de los mismos o revocar el consentimiento que para tal fin nos haya otorgado, a través de los procedimientos que hemos implementado.</p>
+
+                <p>Para ejercer ese derecho envíe su solicitud mediante una nota electrónica al correo: <b>miguel.rivera@imarz.com.mx</b>
+                </p>
+                <p> Asimismo, le informamos que debido a que somos Distribuidores de la marca CONTPAQ i®, sus datos personales pudieran ser transferidos a dicha empresa exclusivamente.</p>
+
+                <p>Cualquier modificación a este aviso de privacidad podrá consultarla nuestra página de internet <b>imarz.com.mx</b> en la opción Aviso de Privacidad</p>
+
+                <p>Fecha última actualización <b> 04/07/2018</b></p>
+
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php include('footer.php'); ?>
